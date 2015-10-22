59cf3dc090546be1469aafa4deaf61527e8fbb3d
Frank Sun <popsun2010@gmail.com>
2015-10-21 23:42:56 -0700

    Add recursion method for reverse linked list algorithm

--- reverse_linkedList.rb
+++ reverse_linkedList.rb
@@ -18,3 +18,25 @@ def reverse_list(head)
     return head
     
 end
+
+#recursive:
+def reverse_list(head, prev=nil, current=nil)
+
+    #initialize prev and current
+    if prev == nil && current == nil
+        prev = nil 
+        current = head
+    end
+    #base case:
+    if current == nil
+        head = prev
+        return head
+    else
+        next_node = current.next
+        current.next = prev
+        prev = current
+        current = next_node
+        return reverse_list(head, prev, current)
+    end
+
+end

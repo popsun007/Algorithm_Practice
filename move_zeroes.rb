5d4920d614aead7bdc6cfbbaac65d9e53d20ecda
Frank Sun <popsun2010@gmail.com>
2015-10-26 21:52:52 -0700

    move_zeroes

---
+++ move_zeroes.rb
@@ -0,0 +1,17 @@
+# Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+# For example, given nums = [0, 1, 0, 3, 12], after calling your function, nums should be [1, 3, 12, 0, 0].
+
+# @param {Integer[]} nums
+# @return {Void} Do not return anything, modify nums in-place instead.
+
+def move_zeroes(nums)
+    count = 0
+    while nums.find_index(0)
+        nums.delete_at(nums.find_index(0))
+        count += 1
+    end
+    count.times do |e|
+        nums.push(0)
+    end
+end

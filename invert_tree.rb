830fdd49e6579ab7ec7ac7091f692938fe073fb9
Frank Sun <popsun2010@gmail.com>
2015-11-04 10:18:06 -0800

    Invert a binary tree

---
+++ invert_tree.rb
@@ -0,0 +1,39 @@
+# Invert a binary tree.
+
+#      4
+#    /   \
+#   2     7
+#  / \   / \
+# 1   3 6   9
+# to
+#      4
+#    /   \
+#   7     2
+#  / \   / \
+# 9   6 3   1
+
+
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val)
+#         @val = val
+#         @left, @right = nil, nil
+#     end
+# end
+
+# @param {TreeNode} root
+# @return {TreeNode}
+
+
+def invert_tree(root)
+    
+    return root if root == nil
+    
+    temp = root.left
+    root.left = invert_tree(root.right)
+    root.right = invert_tree(temp)
+    
+    return root
+    
+end

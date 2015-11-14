a5daecb58e1c269f6ef8b77d3337dad8c9724b30
Frank Sun <popsun2010@gmail.com>
2015-11-13 22:39:25 -0800

    Remove duplicates from sorted list

---
+++ remove_duplicates_from_sorted_list.rb
@@ -0,0 +1,35 @@
+# Remove Duplicates from Sorted List 
+
+# Given a sorted linked list, delete all duplicates such that each element appear only once.
+
+# For example,
+# Given 1->1->2, return 1->2.
+# Given 1->1->2->3->3, return 1->2->3.
+
+
+# Definition for singly-linked list.
+# class ListNode
+#     attr_accessor :val, :next
+#     def initialize(val)
+#         @val = val
+#         @next = nil
+#     end
+# end
+
+# @param {ListNode} head
+# @return {ListNode}
+
+def delete_duplicates(head)
+    if head == nil || head.next == nil
+        return head
+    end
+    current = head
+    while current.next 
+        if current.val == current.next.val
+            current.next = current.next.next
+        else
+            current = current.next
+        end
+    end
+    return head
+end

76053367361f9f4986a5cf655a788dcdf2758d39
Frank Sun <popsun2010@gmail.com>
2015-12-10 14:48:18 -0800

    Hash table insert method

--- hash_table.rb
+++ hash_table.rb
@@ -1,8 +1,8 @@
-class Hash
+class Hash_table
   @storage = []
   @buckets = 8
   @size = 0
-  
+
   def hash(str, buckets)
     hash = 5381
 
@@ -13,4 +13,36 @@ class Hash
 
     return hash % buckets
   end
+
+  def insert(key, value)
+    index = hash(key, @buckets)
+    if @storage[index]
+      @storage[index].each do |tuple|
+        if tuple[0] === key
+          tuple[1] = value
+          @size += 1
+          return 
+        end
+      end
+    else
+      @storage[index] = []
+      @storage[index].push([])
+      @size += 1
+      return 
+    end
+
+  end
+
+  def wang
+    puts "Aoooo"
+  end
 end
+
+bag = Hash_table.new
+
+
+puts bag
+
+bag.insert("tess", "haha")
+
+# bag.wang

2deaafeade2f857efeb64532c2997b2b7ecd463c
Frank Sun <popsun2010@gmail.com>
2016-01-17 22:12:45 -0800

    Add a method to Find last word length

--- lastWordLength.js
+++ lastWordLength.js
@@ -2,24 +2,41 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLastWord = function(s) {
-    if(s === undefined || s.length === 0){
-    	return 0;
-    }
+// var lengthOfLastWord = function(s) {
+//     if(s === undefined || s.length === 0){
+//     	return 0;
+//     }
 
-    result_arr = s.split(" ");
-    lastWordIndex = result_arr.length - 1;
+//     result_arr = s.split(" ");
+//     lastWordIndex = result_arr.length - 1;
 
-    while(lastWordIndex >= 0){
-    	if(result_arr[lastWordIndex] === ""){
-    		lastWordIndex--;
-    	}
-    	else{
-    		return result_arr[lastWordIndex].length;
-    	}
-    }
+//     while(lastWordIndex >= 0){
+//     	if(result_arr[lastWordIndex] === ""){
+//     		lastWordIndex--;
+//     	}else{
+//     		return result_arr[lastWordIndex].length;
+//     	}
+//     }
 
-    return 0;
+//     return 0;
+// };
+
+var lengthOfLastWord = function(s){
+	if(s === undefined || s.length === 0){
+		return 0;
+	}
+
+	var count = 0;
+
+	for(var i = s.length - 1; i >= 0; i--){
+		if(s[i] === " " && count !== 0){
+			break;
+		}else if(s[i] != " "){
+			count++;
+		}
+	}
+
+	return count;
 };
 
-console.log(lengthOfLastWord("   Hello  world "));
+console.log(lengthOfLastWord("   hello world    "));

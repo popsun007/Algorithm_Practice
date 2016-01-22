9804d017b8a39534fe08eb34a5d58c3b540a5e8c
Frank Sun <popsun2010@gmail.com>
2016-01-21 21:58:56 -0800

    Add O(n)time, O(1)space complexity method to isPalindrome

--- PalindromeLinkedList.js
+++ PalindromeLinkedList.js
@@ -1,23 +1,99 @@
+// Given a singly linked list, determine if it is a palindrome.
+
+// Follow up:
+// Could you do it in O(n) time and O(1) space?
+
+ // * Definition for singly-linked list.
+ function ListNode(val) {
+    this.val = val;
+    this.next = null;
+ }
+ 
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+
+//Method 1:
+
+// function isPalindrome(head){
+// 	if(head === null){
+// 		return true;
+// 	}
+
+// 	var list = [],
+// 		current = head;
+
+// 	while(current !== null){
+// 		list.push(current.val);
+// 		current = current.next;
+// 	}
+
+// 	for(var i = 0; i < list.length / 2; i++){
+// 		last_index = list.length - 1 - i;
+// 		if(list[i] !== list[last_index]){
+// 			return false;
+// 		}
+// 	}
+
+// 	return true;
+// }
+
+//Method 2: O(n) time and O(1) space
 function isPalindrome(head){
 	if(head === null){
 		return true;
 	}
 
-	var list = [],
-		current = head;
+//1: find middle node
+	var middle = head,
+		fast = head;
+
+	while(fast !== null && fast.next !== null && fast.next.next !== null){
+		fast = fast.next.next;
+		middle = middle.next;
+	}
+
+//2: reverse the latter half of linked list
+	var compare_list = reverseLinkedList(middle.next);
+
+//3: check is palindrome or not
+	var result = true;
+	while(compare_list !== null){
+		if(head.val !== compare_list.val){
+			result = false;
+		}
+		head = head.next;
+		compare_list = compare_list.next;
+	}
+
+	return result;
+}
+
+function reverseLinkedList(head){
+	if(head === null || head.next === null){
+		return head;
+	}
+
+	var current = head,
+		prev = null;
 
 	while(current !== null){
-		list.push(current.val);
-		current = current.next;
+		var next_node = current.next;
+		current.next = prev;
+		prev = current;
+		current = next_node;
 	}
 
-	for(var i = 0; i < list.length / 2; i++){
-		last_index = list.length - 1 - i;
-		if(list[i] !== list[last_index]){
-			return false;
-		}
-	}
-
-	return true;
+	return prev;
 }
+
+headNode = new ListNode(1);
+headNode.next = new ListNode(2);
+headNode.next.next = new ListNode(2);
+headNode.next.next.next = new ListNode(1);
+
+console.log(isPalindrome(headNode));
+
+
 

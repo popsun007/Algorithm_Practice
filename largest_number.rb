e629c68484f183a491a67227f1b83df2f24a3a4e
Frank Sun <popsun2010@gmail.com>
2016-01-30 14:54:15 -0800

    Add a sorting method to largest_number

--- largest_number.rb
+++ largest_number.rb
@@ -1,7 +1,17 @@
 # @param {Integer[]} nums
 # @return {String}
 def largest_number(nums)
+  if nums.nil? || nums.length === 0
+    return ""
+  end
   nums.permutation.to_a.map { |e| e.join("").to_i }.sort[-1].to_s
 end
 
+def largest_number(nums)
+  if nums.nil? || nums.length === 0
+    return ""
+  end
+  
+  nums.map(&:to_s).sort{|a, b| a + b <=> b + a}.reverse.join("").to_i.to_s
+end
 print largest_number([3, 30, 34, 5, 9])

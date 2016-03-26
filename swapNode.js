36b33dcf9585937d357005d7b6170da18a9d5ceb
Frank Sun <popsun2010@gmail.com>
2016-03-25 22:36:31 -0700

    Swap linked list nodes

---
+++ swapNode.js
@@ -0,0 +1,30 @@
+// Given a linked list, swap every two adjacent nodes and return its head.
+
+// For example,
+// Given 1->2->3->4, you should return the list as 2->1->4->3.
+
+// Your algorithm should use only constant space. You may not modify the values in the list, only nodes itself can be changed.
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function(head) {
+    if(head === null || head.next === null){
+        return head;
+    }
+    
+    var temp = head.next;
+    head.next = swapPairs(head.next.next);
+    temp.next = head;
+    
+    return temp;
+};

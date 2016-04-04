aa975898ae7aca6a6b545e418143efe793e017a2
Frank Sun <popsun2010@gmail.com>
2016-04-03 22:13:51 -0700

    Rewrite StrStr method

--- stringString.js
+++ stringString.js
@@ -19,6 +19,28 @@ function strStr(string1, string2){
 	return 0;
 }
 
+var strStr = function(haystack, needle) {
+    if(needle === undefined || needle.length === 0){
+        return 0;
+    }
+    if(haystack === undefined){
+        return -1;
+    }
+    
+    for(var hay_id = 0; hay_id <= haystack.length - needle.length; hay_id++){
+        for(var ned_id = 0; ned_id < needle.length; ned_id++){
+            if(haystack[hay_id + ned_id] !== needle[ned_id]){
+                break;
+            }
+        }
+        if(ned_id === needle.length){
+            return hay_id;
+        }
+    }
+    
+    return -1;
+};
+
 var a = "string";
 var b = "abc";
 var c = "ring";
